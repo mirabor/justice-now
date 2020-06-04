87ad9af2ccac3b3dc49e71c51ca1382b03ac2d1a
Mira Yu <mirabear@gmail.com>
2020-06-04 16:10:47 -0400

    Initial Commit

---
+++ Demand/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Demand
+//
+//  Created by Mira Yu on 6/4/20.
+//  Copyright © 2020 Mira Yu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
